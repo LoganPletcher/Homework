6cc73bc8a4fe13568f90f925b6f9e736ab4dce74
LoganPletcher <lpletch95@hotmail.com>
2015-09-01 18:00:04 -0500

    Text_Based_RPG update

---
+++ Text_Based_RPG/Text_Based_RPG/Source.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Player
+{
+public:
+	Player();
+	~Player();
+
+private:
+
+};
+
+Player::Player()
+{
+}
+
+Player::~Player()
+{
+}
+
+class Warrior
+{
+public:
+	Warrior();
+	~Warrior();
+
+private:
+
+};
+
+Warrior::Warrior()
+{
+}
+
+Warrior::~Warrior()
+{
+}
+
+class Rogue
+{
+public:
+	Rogue();
+	~Rogue();
+
+private:
+
+};
+
+Rogue::Rogue()
+{
+}
+
+Rogue::~Rogue()
+{
+}
+
+class Mage
+{
+public:
+	Mage();
+	~Mage();
+
+private:
+
+};
+
+Mage::Mage()
+{
+}
+
+Mage::~Mage()
+{
+}
+
+int main()
+{
+	return 0;
+}
