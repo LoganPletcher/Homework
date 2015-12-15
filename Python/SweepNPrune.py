f34a472930378aeda2483d055534544b66682089
LoganPletcher <lpletch95@hotmail.com>
2015-12-15 17:50:06 -0600

    Python Commit 5

---
+++ Python/SweepNPrune.py
@@ -0,0 +1,13 @@
+import Vector Class
+from Vector Class import *
+
+object1 = Vector2D(5, 4)
+obj1xMax = object1.x + 20
+
+object2 = Vector2D(8, 9)
+obj2xMax = object2.x + 75
+
+object3 = Vector2D(17, 7)
+obj3xMax = object3.x + 30
+
+object4 = Vector2D(50, 7)
