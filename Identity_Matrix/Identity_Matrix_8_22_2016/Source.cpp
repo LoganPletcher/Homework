18e1b9f9fd43e02b20cfa5695b042132c9a65cb2
LoganPletcher <lpletch95@hotmail.com>
2016-08-28 23:01:38 -0500

    Finished the ID Matrix creation program.

--- Identity_Matrix/Identity_Matrix_8_22_2016/Source.cpp
+++ Identity_Matrix/Identity_Matrix_8_22_2016/Source.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
 
+void IDMatrixCreator(int num, int *mat)
+{
+	int oneCount = 0;
+	for (int i = 0; i < num*num; i+=num)
+	{
+		for (int j = i; j < i+num; j++)
+		{
+			if (j == i + oneCount)
+				mat[j] = 1;
+			else
+				mat[j] = 0;
+		}
+		oneCount++;
+	}
+}
+
 int main()
 {
-	int IDMatrix[2][2] = { {1, 0},{0, 1} };
-	std::cout << IDMatrix[0][0] << " " << IDMatrix[0][1] << std::endl << IDMatrix[1][0] << " " << IDMatrix[1][1] << std::endl;
+	int IDMatrix[9];
+	IDMatrixCreator(3, IDMatrix);
+	for (int i = 0; i < 9; i += 3)
+	{
+		for (int j = i; j < i + 3; j++)
+			std::cout << IDMatrix[j] << ",";
+		std::cout << std::endl;
+	}
 	system("pause");
 	return 0;
 }
