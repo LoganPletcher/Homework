bb49f0dcddef6c5c5ebe75ee69e74949acf33759
Logan Pletcher <lpletch95@hotmail.com>
2016-08-31 13:09:00 -0500

    Updated Prime Numbers

--- 8_31_2016_PrimeNumbers/8_31_2016_PrimeNumbers/Source.cpp
+++ 8_31_2016_PrimeNumbers/8_31_2016_PrimeNumbers/Source.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
 
-bool PrimeNumChecker(const int &num)
+bool MultipleChecker(const int &num)
 {
-	bool PrimeCheck = true;
-	for (int i = 1; i < 10; i++)
+	for (int i = 2; i < num; i++)
 	{
-		if (!((i == 1)||(i == num)))
-			if(num % i == 0)
-				PrimeCheck = false;
+		if (num % i == 0)
+			return true;
 	}
-	return PrimeCheck;
+	return false;
 }
 
-void PrimesUnder100(bool (*function)(const int &))
+bool PrimeNumChecker(const int &num)
+{
+	if (num == 1 || num == 2)
+		return true;
+	if (!MultipleChecker(num))
+		return true;
+	return false;
+}
+
+void PrimesUnder100(bool(*function)(const int &))
 {
 	for (int i = 0; i < 100; i++)
 	{
@@ -21,60 +28,19 @@ void PrimesUnder100(bool (*function)(const int &))
 	}
 }
 
-void NULLING(const int &size, int *intarray)
+void PnMUnder1000(bool(*pFunction)(const int &))
 {
-	for (int i = 0; i < size; i++)
+	for (int i = 0; i < 1000; i++)
 	{
-		intarray[i] = NULL;
-	}
-}
-
-bool MultipleChecker(const int &num, const int &y, int *intarray)
-{
-	int arraycounter = 0;
-	bool MultipleCheck = false;
-	for (int i = 1; i < y+1; i++)
-	{
-		if ((i > num) && (i % num == 0))
-		{
-			MultipleCheck = true;
-			intarray[arraycounter] = i;
-			arraycounter++;
-		}
-	}
-	return MultipleCheck;
-}
-
-void PnMUnder1000(bool(*pFunction)(const int &), bool(*mFunction)(const int &,const int &, int *), void (*NULLER)(const int &, int *))
-{
-	int multipleholder[1000];
-	int size = (sizeof(multipleholder) / sizeof(*multipleholder));
-	for (int i = 0; i < size; i++)
-	{
-		
 		if (pFunction(i))
-		{
-			NULLER(size, multipleholder);
-			if (mFunction(i, size, multipleholder))
-			{
-				std::cout << i << " is a prime number and is a multiple of the following numbers : ";
-				for (int i = 0; i < size; i++)
-				{
-					if (multipleholder[i] != NULL)
-						std::cout << multipleholder[i] << ", ";
-				}
-				std::cout << "\n\n";
-			}
-			else
-				std::cout << i << " is a prime number and isn't a multiple of any numbers under 1000.\n\n";
-		}
+			std::cout << i << " is a prime number.\n";
 	}
 }
 
 int main()
 {
 	PrimesUnder100(PrimeNumChecker);
-	PnMUnder1000(PrimeNumChecker, MultipleChecker, NULLING);
+	PnMUnder1000(PrimeNumChecker);
 	system("pause");
 	return 0;
 }
